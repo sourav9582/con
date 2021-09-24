f2284d808318be94e40f1bd36f7e4387bddb2cc8
Sourav <ritik991067@gmail.com>
2021-09-24 20:23:41 +0530

    intial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Footer() {
+    return (
+      <>
+
+
+     
+<footer className="page-footer font-small blue bg-blue">
+
+  
+  <div className="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+  </div>
+  
+
+</footer>
+
+      </>
+    );
+  }
+  
+  export default Footer;
---
+++ src/Home.jsx
@@ -0,0 +1,20 @@
+
+import React from "react";
+import './Home.css';
+
+function Home() {
+  return (
+   <>
+    <div id='ggg'>
+        <h1><strong>Covenants IT Sloutions Private Limited</strong></h1>
+        <h2>Startup company </h2>
+        <span >
+            We are a startup company working on AI-based chatbots. We intend to build personalized chatbots APIs that are easy to integrate into websites.
+        </span>
+       
+    </div>
+   </>
+  );
+}
+
+export default Home;
